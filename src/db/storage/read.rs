acbc89cdff4053e80c7ae1dfee6f07734677d4e4
pedrinfx <canaldopedrinho233@gmail.com>
2022-04-23 20:12:07 -0300

    fix: change return on unshard module

--- src/db/storage/read.rs
+++ src/db/storage/read.rs
@@ -22,19 +22,18 @@ pub fn document(name: String, database_path: String) -> Result<Document, &'stati
         return Err("Document not found");
     }
 
-    let mut document_content: Vec<Vec<u8>> = Vec::new();
-    let mut index = 0;
+    let mut document_content: Vec<sharding::unsharding_document::DocumentShard> = Vec::new();
     for document_path in documents_path {
-        if document_path.ends_with(index.to_string().as_str()) {
-            let content = fs::read(Path::new(&database_path).join(document_path)).unwrap();
-            document_content.push(content);
-            index += 1;
-        }
+        let content = fs::read(Path::new(&database_path).join(document_path.clone())).unwrap();
+        document_content.push(sharding::unsharding_document::DocumentShard {
+            content,
+            name: document_path,
+        });
     }
 
     let string_document = sharding::unsharding_document::unshard(document_content);
 
-    let document: Document = serde_json::from_str(&string_document).unwrap();
+    let document: Document = serde_json::from_str(&string_document).expect("Document is not valid or failed on unsharding");
 
     return Ok(document);
 }
