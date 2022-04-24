5877543b52c0db4b8d9d4aee73ef226bd7052928
pedrinfx <canaldopedrinho233@gmail.com>
2022-04-23 21:13:00 -0300

    test(database.rs): added unit tests for database module

--- src/db/database.rs
+++ src/db/database.rs
@@ -52,3 +52,67 @@ impl Database {
         unimplemented!();
     }
 }
+
+// Unit Tests
+#[cfg(test)]
+mod database_test {
+    use super::*;
+    use crate::db;
+    use std::fs;
+    use std::path;
+
+    const TEST_DB_PATH: &str = "./data/test_db";
+
+    #[test]
+    fn create_database() {
+        let database_path = path::Path::new(TEST_DB_PATH);
+
+        if database_path.exists() {
+            fs::remove_dir_all(database_path).expect("Failed to remove database path");
+        }
+
+        let _database = Database::new(database_path.to_str().unwrap().to_string());
+
+        assert!(database_path.exists());
+    }
+
+    #[test]
+    fn create_document() {
+        let database_path = path::Path::new(TEST_DB_PATH);
+
+        let database = Database::new(database_path.to_str().unwrap().to_string());
+
+        let document_name = "test_create_document".to_string();
+        let data = vec![storage::types::Data::new(
+            "test_data".to_string(),
+            db::storage::types::Types::String("test_data".to_string()),
+        )];
+
+        database
+            .create_document(document_name.clone(), data)
+            .unwrap();
+
+        assert!(database_path.join(document_name + "_0").exists());
+    }
+
+    #[test]
+    fn get_document()  {
+        let database_path = path::Path::new(TEST_DB_PATH);
+
+        let database = Database::new(database_path.to_str().unwrap().to_string());
+
+        let document_name = "test_get_document".to_string();
+        let data = vec![storage::types::Data::new(
+            "test_data".to_string(),
+            db::storage::types::Types::String("test_data".to_string()),
+        )];
+
+        database
+            .create_document(document_name.clone(), data)
+            .unwrap();
+
+        let document = database.get_document(document_name.clone()).unwrap();
+
+        assert_eq!(document.name, document_name);
+    }
+}
