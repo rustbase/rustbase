5108f4ecb4ff885643fc9d7a6e388a733bd97c1d
peeeuzin <canaldopedrinho233@gmail.com>
2023-04-09 12:17:25 -0300

    fix(build): removing windows icon

--- src/build.rs
+++ src/build.rs
@@ -1,12 +1,4 @@
 fn main() {
-    if cfg!(windows) {
-        extern crate winres;
-        let mut winresouces = winres::WindowsResource::new();
-
-        winresouces.set_icon("./assets/icon.ico");
-        winresouces.compile().unwrap();
-    }
-
     println!(
         "cargo:rustc-env=VERSION_CODE={}",
         std::env::var("VERSION_CODE").unwrap()
